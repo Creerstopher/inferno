9f0f3429e9a9eea9905d6595e19bfcad64d46b93
Creerstopher <zagiduulin@vk.com>
2024-02-19 17:14:54 +0300

    second commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,11 @@ import './Card.css'
 import like from '../../../public/ico/like.png'
 import { NavLink } from "react-router-dom";
 
-const Card = ({id, name, price, firstPhoto }) => {
+const Card = ({id, name, price, photo }) => {
     return (
         <>
             <div className="card">
-                <div style={{ backgroundImage: `url(${firstPhoto})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="card_img">
+                <div style={{ backgroundImage: `url(${photo})`, backgroundSize: 'cover', backgroundPosition: 'center' }} className="card_img">
                     <div className="card_btns">
                          <NavLink to={`/catalog/${id}`}> <div className="add_basket"> <p> В КОРЗИНУ</p></div></NavLink>
                         <div className="add_like"><img src={like} alt="like" className="like" /></div>
